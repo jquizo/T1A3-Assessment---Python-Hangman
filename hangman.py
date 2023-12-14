c64443ff6d32c4840369dc8c36e04ce56285e26f
jquizo <45918580+jquizo@users.noreply.github.com>
2023-12-14 22:21:55 +1100

    install and import colorama package

--- hangman.py
+++ hangman.py
@@ -1,5 +1,10 @@
 import random
+import colorama 
+
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 from words import word_list
+
 
 def get_random_word():
     word = random.choice(word_list)
