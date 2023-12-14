69a7d975676d4d62e7d36792845506be975bc619
jquizo <45918580+jquizo@users.noreply.github.com>
2023-12-14 23:08:41 +1100

    import pyfiglet

--- hangman.py
+++ hangman.py
@@ -1,12 +1,31 @@
 import random
 import colorama 
+import pyfiglet
 
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 from words import word_list
+from pyfiglet import figlet_format
 
 
 def get_random_word():
     word = random.choice(word_list)
     return word.upper()
 
+
+def play_hangman(word):
+    word_completion = "_" * len(word)
+    guessed_words = False
+    guessed_letters = []
+    guessed_words = []
+    tries_left = 6
+    print(pyfiglet.figlet_format("Hangman"))
+    print(word_completion)
+    print("\n")
+
+
+def main():
+    word = get_random_word()
+    play_hangman(word)
+
+    
