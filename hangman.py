604113e6d7467f5ca680500e9b2e07b35896d97a
jquizo <45918580+jquizo@users.noreply.github.com>
2023-12-15 01:53:12 +1100

    added input for guessing word

--- hangman.py
+++ hangman.py
@@ -14,15 +14,21 @@ def get_random_word():
 
 
 def play_hangman(word):
-    word_completion = "_" * len(word)
-    guessed_words = False
-    guessed_letters = []
-    guessed_words = []
-    tries_left = 6
-    print(pyfiglet.figlet_format("Hangman"))
-    print(word_completion)
-    print(hangman_display(tries_left))
-    print("\n")
+   word_completion = "_" * len(word)
+   guessed = False
+   guessed_letters = []
+   guessed_words = []
+   tries_left = 6
+   score = 0  
+   print(pyfiglet.figlet_format("Hangman"))
+   print("+10 score for each LETTER guessed. 50 points for guessing the correct word!!")
+   print(word_completion)
+   print(hangman_display(tries_left))
+   print("\n")
+   # Initialise game loop
+   while not guessed and tries_left > 0:
+        # Get user input
+        guess = input(Fore.BLUE + "Please guess a letter or word: ").upper()
 
 def hangman_display(tries_left):
     stages = [  # head, torso, both arms, and both legs
