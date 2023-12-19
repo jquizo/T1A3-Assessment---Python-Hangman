3782dc8cf157e02ba5c12087ffb1009e451f6ad9
jquizo <45918580+jquizo@users.noreply.github.com>
2023-12-20 03:06:08 +1100

    fixed indentations

--- hangman.py
+++ hangman.py
@@ -1,11 +1,10 @@
 import random
 import colorama
 import pyfiglet
-import os
-import json
 
 from words import word_list
 from colorama import Fore
+# Initialize colorama
 colorama.init(autoreset=True)
 from pyfiglet import figlet_format
 
@@ -27,59 +26,66 @@ def play(word):
     guessed_letters = []
     guessed_words = []
     tries_left = 6
-    score = 0  
+    score = 0
     print(pyfiglet.figlet_format("Hangman"))
     print("+10 score for each LETTER guessed. 50 points for guessing the correct word!!")
     print(word_completion)
     print(hangman_display(tries_left))
     print("\n")
+
     # Initialise game loop
     while not guessed and tries_left > 0:
         guess = input(Fore.BLUE + "Please guess a letter or word: ").upper()
+
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
-                print(Fore.RED + "You already guessed the letter", guess)
+                print(Fore.RED + f"You already guessed the letter {guess}")
             elif guess not in word:
-                print(Fore.RED + guess, Fore.RED + "is not in the word.")
+                print(Fore.RED + f"{guess} is not in the word.")
                 tries_left -= 1
                 guessed_letters.append(guess)
             else:
-                # Correct letter guess
-                print(Fore.GREEN + "Good job,", Fore.GREEN + guess, Fore.GREEN + "is in the word!")
-                score += 10  # Add points for each correct letter
+                print(Fore.GREEN + f"Good job, {guess} is in the word!")
+                score += 10
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
                 indices = [i for i, letter in enumerate(word) if letter == guess]
+
                 for index in indices:
                     word_as_list[index] = guess
+
                 word_completion = "".join(word_as_list)
+
                 if "_" not in word_completion:
                     guessed = True
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
-                print(Fore.RED + "You already guessed the word", guess)
+                print(Fore.RED + f"You already guessed the word {guess}")
             elif guess != word:
-                print(Fore.RED + guess, "is not the word.")
+                print(Fore.RED + f"{guess} is not the word.")
                 tries_left -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
                 word_completion = word
-                score += 50  # Add points for guessing the whole word
+                score += 50
         else:
             print(Fore.RED + "Not a valid guess.")
+
         print(hangman_display(tries_left))
         print(word_completion)
         print("\n")
+
     if guessed:
         print(Fore.GREEN + pyfiglet.figlet_format("You Win!"))
         print(Fore.GREEN + f"Congrats, you guessed the word! Score: {score}")
         player_name = input("Enter your name for the leaderboard: ")
-        leaderboard[player_name] = score  # Add the player's score to the leaderboard
+        leaderboard[player_name] = score
         display_leaderboard()
     else:
         print(Fore.RED + pyfiglet.figlet_format("You Lose!"))
         print(Fore.RED + f"Sorry, you ran out of tries. The word was {word}. Better luck next time! Score: {score}")
+
 
 def hangman_display(tries_left):
     stages = [  # head, torso, both arms, and both legs
@@ -161,14 +167,14 @@ def hangman_display(tries_left):
 def main():
     word = get_word()
     play(word)
+
     while True:
         play_again = input("Would you like to play again? (Y/N) ").upper()
-        if play_again == "N":
+
+        if play_again == "N" or (play_again != "Y" and play_again != "N"):
             print("Thanks for playing!")
             break
-        elif play_again != "Y":
-            print("Thanks for playing!")
-            break
+
         word = get_word()
         play(word)
 
